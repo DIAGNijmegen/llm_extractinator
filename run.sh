b5619dec0c98bb474fa17c4fe0033d25b2abfe2d
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-10-17 14:19:17 +0000

    bugfix

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-extract_data --datapath /data/bodyct/experiments/luc_t10162/DRAGON --task_ids 1
+extract_data \
+    --datapath /data/bodyct/experiments/luc_t10162/DRAGON \
+    --task_ids 1 \
+    --num_examples 1 \
