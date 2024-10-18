8ef167500dc865055d39313befbccee8ca56f7ac
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-10-18 14:47:26 +0000

    changed run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 pip install -e .
 
 extract_data \
-    --data_path /data/bodyct/experiments/luc_t10162/DRAGON \
-    --ground_truth_path /data/bodyct/experiments/luc_t10162/DRAGON/debug-test-set \
-    --task_ids 1 \
-    --num_examples 0\
-    --model_name qwen2.5:1.5b
+    --task_id 1 \
+    --model_name phi3.5
