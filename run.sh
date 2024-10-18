b6022fc43c2d5e12ea654ee636941c78d6af4b79
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-10-18 09:05:35 +0000

    update project setup

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+pip install -e .
+
 extract_data \
     --datapath /data/bodyct/experiments/luc_t10162/DRAGON \
     --task_ids 1 \
-    --num_examples 1
+    --num_examples 0\
+    --model_name qwen2.5:0.5b
