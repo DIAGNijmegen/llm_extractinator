b8e6b5a6f2cdb6b23ee0e72a571c3e66452d3e9f
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-10-21 09:40:49 +0000

    updated run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ pip install -e .
 
 MODEL_NAME="phi3.5"
 NUM_EXAMPLES=0
-RUN_NAME="$MODEL_NAME/$NUM_EXAMPLES"
+RUN_NAME="${MODEL_NAME}/${NUM_EXAMPLES}_examples"
 
 extract_data \
     --task_id 1 \
@@ -14,4 +14,7 @@ extract_data \
 
 evaluate \
     --task_ids 1 \
-    --prediction_path /output/$RUN_NAME
+    --prediction_path /output/$RUN_NAME \
+    --ground_truth_path /data/bodyct/experiments/luc_t10162/DRAGON/debug-test-set \
+    --output_path /output/$RUN_NAME
+
