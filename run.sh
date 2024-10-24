17176d72f854057f31fd972f8f22b5650aa52653
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-10-24 14:47:01 +0000

    Updated tasks

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# pip install -e .
+pip install -e .
 
 BASE_DIR="/data/bodyct/experiments/luc_t10162/GitHub/LLM_data_extractor"
 GROUND_TRUTH_PATH="/data/bodyct/experiments/luc_t10162/DRAGON/debug-test-set"
@@ -11,13 +11,13 @@ PREDICTION_PATH="${BASE_DIR}/output/${RUN_NAME}"
 OUTPUT_PATH="${PREDICTION_PATH}/metrics.json"
 
 extract_data \
-    --task_id 1 \
+    --task_id 14 \
     --model_name $MODEL_NAME \
     --num_examples $NUM_EXAMPLES \
     --run_name $RUN_NAME
 
 evaluate \
-    --task_ids 1 \
+    --task_ids 14 \
     --prediction_path $PREDICTION_PATH \
     --ground_truth_path $GROUND_TRUTH_PATH \
     --output_path $PREDICTION_PATH
