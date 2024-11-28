87ddd7cc387682dd2a3175e6514a904593213fec
Luc Builtjes <luc.builtjes@radboudumc.nl>
2024-11-28 10:26:52 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 
 pip install -e .
 
-# for NUM_EXAMPLES in 0 1 3 5
-# do
+for MODEL_NAME in "mistral-nemo" "llama3.1" "llama3.1:70b" "gemma2" "phi3" "llama3.2" "qwen2" "mixtral"
+do
+for task_id in 2 18
+do
 BASE_DIR="/data/bodyct/experiments/luc_t10162/GitHub/LLM_data_extractor"
 GROUND_TRUTH_PATH="/data/bodyct/experiments/luc_t10162/DRAGON/debug-test-set"
-MODEL_NAME="mistral-nemo"
+# MODEL_NAME="mistral-nemo"
 NUM_EXAMPLES=0
-RUN_NAME="translation_test/${MODEL_NAME}/${NUM_EXAMPLES}_examples"
+RUN_NAME="all_models_test/${MODEL_NAME}/${NUM_EXAMPLES}_examples"
 PREDICTION_PATH="${BASE_DIR}/output/${RUN_NAME}"
 OUTPUT_PATH="${PREDICTION_PATH}/metrics.json"
 
@@ -30,16 +32,28 @@ OUTPUT_PATH="${PREDICTION_PATH}/metrics.json"
 #     --output_path $OUTPUT_PATH
 
 
-# for task_id in {1..24}
-# do
-#     extract_data \
-#         --task_id $task_id \
-#         --model_name $MODEL_NAME \
-#         --num_examples $NUM_EXAMPLES \
-#         --run_name $RUN_NAME \
-#         --max_context_len 15000 \
-#         --translate
-# done
+
+extract_data \
+    --task_id $task_id \
+    --model_name $MODEL_NAME \
+    --num_examples $NUM_EXAMPLES \
+    --run_name $RUN_NAME \
+    --max_context_len 15000 \
+    --num_predict 8192 \
+    --translate
+done
+
+post_process \
+    --output_path $PREDICTION_PATH \
+    --task_ids 2 18
+
+evaluate \
+    --task_ids 2 18 \
+    --prediction_path $PREDICTION_PATH \
+    --ground_truth_path $GROUND_TRUTH_PATH \
+    --output_path $OUTPUT_PATH
+done
+
 
 # for task_id in {25..28}
 # do
@@ -52,13 +66,13 @@ OUTPUT_PATH="${PREDICTION_PATH}/metrics.json"
 #         --max_context_len 8192
 # done
 # done
-post_process \
-    --output_path $PREDICTION_PATH \
-    --task_ids 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+# post_process \
+#     --output_path $PREDICTION_PATH \
+#     --task_ids 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 
-evaluate \
-    --task_ids 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 \
-    --prediction_path $PREDICTION_PATH \
-    --ground_truth_path $GROUND_TRUTH_PATH \
-    --output_path $OUTPUT_PATH
+# evaluate \
+#     --task_ids 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 \
+#     --prediction_path $PREDICTION_PATH \
+#     --ground_truth_path $GROUND_TRUTH_PATH \
+#     --output_path $OUTPUT_PATH
 
